8746af60c11d3eef18b8ec69ade6cfc85983212b
BlaBlaArt <ivanovartemw@gmail.com>
2020-09-23 15:50:46 +0300

    Thanks YouTube(Emerald Powder)

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public Renderer MainRenederer;
+    public Vector2Int Size = Vector2Int.one;
+
+    public void SetTransparrent( bool available)
+    {
+        if(available)
+           MainRenederer.material.color = Color.green;
+        else
+           MainRenederer.material.color = Color.red;
+    }
+
+    public void SetNormal()
+    {
+        MainRenederer.material.color = Color.white;
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        for(int x = 0; x < Size.x; x++)
+        {
+            for (int y = 0; y < Size.y; y++)
+            {
+                if((x + y) % 2 == 0) Gizmos.color = Color.yellow;
+                else Gizmos.color = Color.blue;
+
+                Gizmos.DrawCube(transform.position + new Vector3(x, 0, y) , new Vector3(1,.1f,1));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/BuildingsGrid.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingsGrid : MonoBehaviour
+{
+    public Vector2Int GridSize = new Vector2Int(10, 10);
+
+    private Building[,] grid;
+
+    private Building flyingBuilding;
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        grid = new Building[GridSize.x, GridSize.y];
+
+        mainCamera = Camera.main;
+    }
+
+    public void StartPlacingBuilding(Building BuildingPref)
+    {
+        if(flyingBuilding != null)
+        {
+            Destroy(flyingBuilding.gameObject);
+        }
+
+        flyingBuilding = Instantiate(BuildingPref);
+    }
+
+    private void Update()
+    {
+        if(flyingBuilding != null)
+        {
+            var _groundPlace = new Plane(Vector3.up, Vector3.zero);
+
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            if(_groundPlace.Raycast(ray, out float position))
+            {
+                Vector3 worldPosition = ray.GetPoint(position);
+
+                int x = Mathf.RoundToInt(worldPosition.x);
+                int y = Mathf.RoundToInt(worldPosition.z);
+
+                bool available = true;
+
+                if (x < 0 || x > GridSize.x - flyingBuilding.Size.x) available = false;
+                if (y < 0 || y > GridSize.y - flyingBuilding.Size.y) available = false;
+
+                if(available && isPlaceTaken(x, y))
+                {
+                    available = false;
+                }
+
+                flyingBuilding.transform.position = new Vector3(x, 0, y);
+                flyingBuilding.SetTransparrent(available);
+
+                if (available && Input.GetMouseButtonDown(0))
+                {
+                    PlaceFlyingBuiding(x,y);
+                }
+            }
+        }
+    }
+
+    private bool isPlaceTaken(int Place_x, int Place_y)
+    {
+        for (int x = 0; x < flyingBuilding.Size.x; x++)
+        {
+            for (int y = 0; y < flyingBuilding.Size.y; y++)
+            {
+                if(grid[Place_x + x, Place_y + y] != null)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
+
+    private void PlaceFlyingBuiding(int Place_x, int Place_y)
+    {
+        for (int x = 0; x < flyingBuilding.Size.x; x++)
+        {
+            for (int y = 0; y < flyingBuilding.Size.y; y++)
+            {
+                grid[Place_x + x, Place_y + y] = flyingBuilding;
+            }
+        }
+
+        flyingBuilding.SetNormal();
+        flyingBuilding = null;
+    }
+
+}
